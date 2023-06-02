86831be0d5e2603ca02fe34ec1d13adf9c1e8e9b
Qazi Amaan <qazi9amaan@gmail.com>
2023-06-03 02:38:02 +0530

    fix(style): updated style

--- src/lib/components/atoms/FormHeader.tsx
+++ src/lib/components/atoms/FormHeader.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const FormHeader = ({ style, title, subTitle }: Props) => {
   return (
-    <div className='form--row !mt-2' style={style}>
+    <div className='form--row !mt-4' style={style}>
       <section className='form--section'>
         <h2 className='form--title'>{title}</h2>
         {!isNA(subTitle) && <p className=' text-gray-500 text-sm mt-0 leading-tight'>{subTitle}</p>}
