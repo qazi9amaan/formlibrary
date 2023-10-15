2549213b57c0581a628b970d092f5b789cd052d9
Qazi Amaan <qazi9amaan@gmail.com>
2023-10-15 19:49:10 +0530

    fix(types): added formik array

--- src/lib/components/atoms/FormElements/FormArray.tsx
+++ src/lib/components/atoms/FormElements/FormArray.tsx
@@ -6,23 +6,39 @@ type IRenderProps = {
   index: number;
   push: (obj: any) => void;
   remove: (index: number) => void;
+  swap: (indexA: number, indexB: number) => void;
+  move: (from: number, to: number) => void;
+  insert: (index: number, value: any) => void;
+  unshift: (value: any) => number;
+  replace: (index: number, value: any) => void;
 };
 
-type Props<T = any[]> = {
+type Props<T = any> = {
   name: string;
   values: T[];
   render: (props: IRenderProps) => JSX.Element;
 };
 
-export const FormArray = ({ name, values, render }: Props) => {
+export const FormArray = <T = any,>({ name, values, render }: Props<T>) => {
   return (
     <FieldArray
       name={name}
-      render={({ push, remove }: any) => (
+      render={({ push, remove, swap, move, insert, unshift, replace }: any) => (
         <>
           {values.map((value, index) => {
             const baseName = `${name}[${index}]`;
-            return render({ baseName, value, index, push, remove });
+            return render({
+              baseName,
+              value,
+              index,
+              push,
+              remove,
+              swap,
+              move,
+              insert,
+              unshift,
+              replace,
+            });
           })}
         </>
       )}
